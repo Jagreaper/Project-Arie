a1a49cebbd1511f3c5932f1aa952bf214d6414cb
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-05 04:20:12 +1000

    Turned echo off for nuget tools for setting variables and creating packages directory

--- Tools/nuget-build.bat
+++ Tools/nuget-build.bat
@@ -1,5 +1,7 @@
+echo off
 set /p Project=Project: 
 if not exist packages mkdir packages
 cd packages
+echo on
 nuget pack ..\..\%Project%\%Project%.csproj -properties Configuration=Release 
 pause
--- Tools/nuget-push.bat
+++ Tools/nuget-push.bat
@@ -1,7 +1,9 @@
+echo off
 set /p Project=Project: 
 set /p Version=Version: 
 set /p Key=Key: 
 if not exist packages mkdir packages
 cd packages 
+echo on
 nuget push %Project%.%Version%.nupkg %Key% -Source https://www.nuget.org/api/v2/package
 pause
