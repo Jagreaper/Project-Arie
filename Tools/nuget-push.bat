15184178f60afc1d2efc421056af3a6547dad5f6
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-05 04:41:14 +1000

    Added automatic build option for nuget-push.bat tool

--- Tools/nuget-push.bat
+++ Tools/nuget-push.bat
@@ -2,8 +2,10 @@ echo off
 set /p Project=Project: 
 set /p Version=Version: 
 set /p Key=Key: 
+set /p Auto=Automatic Pack: 
 if not exist packages mkdir packages
-cd packages 
+if "%auto%" == "n" cd packages
+if "%auto%" == "y" cd "..\%Project%\bin\Release"
 echo on
 nuget push %Project%.%Version%.nupkg %Key% -Source https://www.nuget.org/api/v2/package
 pause
