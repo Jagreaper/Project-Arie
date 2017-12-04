bd84d41518369e931b02a9b7c105bfb399332eb1
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-05 03:05:50 +1000

    Added Radio Button Implementations

---
+++ Jagerts.Arie.Windows.Classic.Controls/PathRadioButton.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Jagerts.Arie.Windows.Classic.Controls
+{
+    /// <summary>
+    /// A dark theme styled custom button
+    /// </summary>
+    public sealed partial class PathRadioButton : System.Windows.Controls.RadioButton
+    {
+        #region Constructor
+
+        public PathRadioButton() => this.InitializeComponent();
+
+        #endregion
+
+        #region Properties
+
+        public static readonly DependencyProperty PathDataProperty = DependencyProperty.Register("PathData", typeof(Geometry), typeof(PathRadioButton), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty PathStyleProperty = DependencyProperty.Register("PathStyle", typeof(Style), typeof(PathRadioButton), new PropertyMetadata(null));
+
+        public Geometry PathData
+        {
+            get => (Geometry)this.GetValue(PathDataProperty);
+            set => this.SetValue(PathDataProperty, value);
+        }
+
+        public Style PathStyle
+        {
+            get => (Style)this.GetValue(PathStyleProperty);
+            set => this.SetValue(PathStyleProperty, value);
+        }
+
+        #endregion
+    }
+}
---
+++ Jagerts.Arie.Windows.Classic.Controls/RadioButton.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Jagerts.Arie.Windows.Classic.Controls
+{
+    /// <summary>
+    /// A dark theme styled custom button
+    /// </summary>
+    public sealed partial class RadioButton : System.Windows.Controls.RadioButton
+    {
+        public RadioButton() => this.InitializeComponent();
+    }
+}
