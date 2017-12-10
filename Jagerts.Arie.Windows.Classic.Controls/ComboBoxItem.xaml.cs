c692822af1307fc72c9c3621c53aa6e198be4b6d
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-10 17:31:43 +1000

    Fully Implemented ComboBox Control

---
+++ Jagerts.Arie.Windows.Classic.Controls/ComboBoxItem.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Jagerts.Arie.Windows.Classic.Controls
+{
+    /// <summary>
+    /// A dark theme styled custom button
+    /// </summary>
+    public sealed partial class ComboBoxItem : System.Windows.Controls.ComboBoxItem
+    {
+        #region Constructor
+
+        public ComboBoxItem() => this.InitializeComponent();
+
+        #endregion
+    }
+}
