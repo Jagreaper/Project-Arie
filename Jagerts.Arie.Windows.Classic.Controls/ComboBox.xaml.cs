3c239f421e44b7afa952daf2bdaa7c9825de3657
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-11 05:42:51 +1000

    Fixed ComboBoxItem content not being added to ComboBox with a built in type converter that converts System ComboBoxItems  to Arie ComboBoxItems for the Arie ComboBox

--- Jagerts.Arie.Windows.Classic.Controls/ComboBox.xaml.cs
+++ Jagerts.Arie.Windows.Classic.Controls/ComboBox.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Jagerts.Arie.Windows.Classic.Controls
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Jagerts.Arie.Windows.Classic.Controls
 {
     /// <summary>
     /// A dark theme styled custom combo box
@@ -6,5 +10,22 @@
     public sealed partial class ComboBox : System.Windows.Controls.ComboBox
     {
         public ComboBox() => this.InitializeComponent();
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+            List<System.Windows.Controls.ComboBoxItem> items = this.Items.SourceCollection.OfType<object>()
+                                                                         .Where(o => o is System.Windows.Controls.ComboBoxItem && !(o is ComboBoxItem))
+                                                                         .Select(o => (System.Windows.Controls.ComboBoxItem)o).Reverse().ToList();
+
+            foreach (System.Windows.Controls.ComboBoxItem item in items)
+            {
+                if (this.Items.Contains(item))
+                {
+                    this.Items.Remove(item);
+                    this.Items.Add(new ComboBoxItem() { Content = item.Content, });
+                }
+            }
+        }
     }
 }
