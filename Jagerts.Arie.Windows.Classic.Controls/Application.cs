79841322cf09d43d612ee66d604e63c5288e6296
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-17 22:07:31 +1000

    Removed Uneeded code

--- Jagerts.Arie.Windows.Classic.Controls/Application.cs
+++ Jagerts.Arie.Windows.Classic.Controls/Application.cs
@@ -12,21 +12,10 @@ namespace Jagerts.Arie.Windows.Classic.Controls
     public class Application : System.Windows.Application
     {
         #region Methods
-        
-        public Application()
-        {
-            ColorSchemes.Default.Apply();
-        }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            ColorSchemes.Default.Apply();
-        }
-
-        protected override void OnLoadCompleted(NavigationEventArgs e)
-        {
-            base.OnLoadCompleted(e);
             ColorSchemes.Default.Apply();
         }
 
