6eddba2718f980eecddb20674cb5ee16eb75ca70
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-18 14:24:07 +1000

    Added Custom ListBox control

---
+++ Jagerts.Arie.Windows.Classic.Controls/Lists/ListBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Jagerts.Arie.Windows.Classic.Controls
+{
+    public class ListBox : System.Windows.Controls.ListBox
+    {
+        #region Constructor
+
+        static ListBox() => DefaultStyleKeyProperty.OverrideMetadata(typeof(ListBox), new FrameworkPropertyMetadata(typeof(ListBox)));
+
+        #endregion
+    }
+}
