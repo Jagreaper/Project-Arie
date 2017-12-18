0cf39677587c26afca4bc550a7736285ca13a147
Jagreaper <jamesalexgrant@hotmail.com>
2017-12-18 10:14:40 +1000

    Added Custom TextBlock and TextBox

---
+++ Jagerts.Arie.Windows.Classic.Controls/Text/TextBlock.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Jagerts.Arie.Windows.Classic.Controls
+{
+    public class TextBlock : System.Windows.Controls.TextBlock
+    {
+        #region Constructor
+
+        static TextBlock() => DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBlock), new FrameworkPropertyMetadata(typeof(TextBlock)));
+
+        #endregion
+    }
+}
---
+++ Jagerts.Arie.Windows.Classic.Controls/Text/TextBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Jagerts.Arie.Windows.Classic.Controls
+{
+    public class TextBox : System.Windows.Controls.TextBox
+    {
+        #region Constructor
+
+        static TextBox() => DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBox), new FrameworkPropertyMetadata(typeof(TextBox)));
+
+        #endregion
+    }
+}
